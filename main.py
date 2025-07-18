74b55ef8d2e28d29ab21ddb00fc3177b422b8772
HusbanTech12 <husbantech08@gmail.com>
2025-07-18 22:20:23 +0500

    half agents as tool integration

--- main.py
+++ main.py
@@ -16,3 +16,33 @@ french_agent = Agent(
     name = 'French Agent',
     instructions= "You translate the user's message to french"
 )
+
+# Main Agents That Routes To The Correct Translation To The Given Languages.
+
+translation_agent = Agent(
+    name='Translator Agent',
+    instructions="""You are a translation assistant. Route the translation request to the correct laguages agent. Use the appropriate tool to convert English text into either Spanish , Korean or French based no the request.
+    """,
+    
+    tools= [ 
+            
+          # Convert these Agents into Tools  
+       spanish_agent.as_tool(
+           tool_name='translate_to_spanish',
+           tool_description="Translate the user's message to Spanish" 
+       ),     
+            
+       korean_agent.as_tool(
+           tool_name='translate_to_korean',
+           tool_description="Translate the user's message to Korean" 
+       ),
+       french_agent.as_tool(
+           tool_name='translate_to_french',
+           tool_description="Translate the user's message to French" 
+       ),     
+    ]
+    
+    
+    
+    
+)
